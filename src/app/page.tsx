dc5c879fdb8c4f688aaa2d6a9f9a16993900d670
matiasayram <matiasayramm@gmail.com>
2026-03-05 00:57:15 -0300

    mejoro estilos del header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center bg-zinc-500 font-sans ">
-      <main className="flex min-h-screen min-w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen w-full items-center justify-center font-sans ">
+      <main className="flex min-h-screen min-w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-background text-foreground sm:items-start">
         <h1 className="text-black">Home</h1>
       </main>
     </div>
--- src/components/custom/MainHeader.tsx
+++ src/components/custom/MainHeader.tsx
@@ -40,6 +40,7 @@ export const headerRoutes = [
   },
 ];
 
+
 export function MainHeader() {
   const [isScrolled, setIsScrolled] = useState(false);
   const { theme, setTheme } = useTheme();
@@ -60,18 +61,14 @@ export function MainHeader() {
 
   return (
     <header
-      className={cn(
-        "fixed top-0 z-50 flex w-full items-center justify-between px-3 py-1 transition-colors duration-300 ",
-        isScrolled ? "bg-black" : "bg-linear-to-b from-black/80 to-transparent"
-      )}
-    >
+      className="fixed top-0 z-50 flex w-full items-center justify-between px-3 py-1 ">
       <div className="flex items-center gap-8">
         <Link href="/" className="text-2xl font-black md:text-3xl tracking-wide">
           <Image  src="/two-eyes-logo.png" alt="Logo" width={50} height={50} />
         </Link>
         <nav className="hidden items-center gap-5 text-sm font-medium text-foreground md:flex">
         {headerRoutes.map((route) => (
-          <Link key={route.href} href={route.href} className="transition-colors hover:text-white">
+          <Link key={route.href} href={route.href} className="transition-colors hover:opacity-80">
             {route.label}
           </Link>
         ))}
@@ -79,41 +76,42 @@ export function MainHeader() {
         </nav>
       </div>
 
-      <div className="flex items-center gap-4 text-white md:gap-6">
-        <Button variant="ghost" size="icon" className="text-white hover:bg-transparent hover:text-white/80">
+      <div className="flex items-center gap-4 text-foreground md:gap-4">
+        <Button variant="ghost" size="icon" className="text-foreground hover:bg-transparent hover:text-foreground/80">
           <Search className="h-5 w-5" />
           <span className="sr-only">Search</span>
         </Button>
-        <Link href="/kids" className="hidden text-sm font-medium hover:text-white/80 md:block">
+        <Link href="/kids" className="hidden text-sm font-medium hover:text-foreground/80 md:block">
           Kids
         </Link>
-        <Button variant="ghost" size="icon" className="hidden text-white hover:bg-transparent hover:text-white/80 md:flex">
+        <Button variant="ghost" size="icon" className="hidden text-foreground hover:bg-transparent hover:text-foreground/80 md:flex">
           <Bell className="h-5 w-5" />
           <span className="sr-only">Notifications</span>
         </Button>
 
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
+          <DropdownMenuTrigger asChild className="cursor-pointer">
             <Button variant="ghost" className="flex group items-center gap-2 p-0 hover:bg-transparent">
               <Avatar className="h-8 w-8 rounded-md">
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                <AvatarFallback className="rounded-md bg-zinc-800 text-white">N</AvatarFallback>
+                <AvatarImage src="" alt="@shadcn" />
+                <AvatarFallback className="rounded-md text-foreground">N</AvatarFallback>
               </Avatar>
-              <ChevronDown className="h-4 w-4 text-white transition-transform duration-200 group-data-[state=open]:rotate-180" />
+              <ChevronDown className="h-4 w-4 text-foreground transition-transform duration-200 group-data-[state=open]:rotate-180" />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="w-48 bg-black/95 text-white border-zinc-800">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator className="bg-zinc-800" />
-            <DropdownMenuItem className="focus:bg-zinc-800 focus:text-white cursor-pointer">
+          <DropdownMenuContent align="end" className="w-30 bg-background text-foreground ">
+            <DropdownMenuLabel className="hover:bg-sidebar-accent cursor-pointer rounded-md">My account</DropdownMenuLabel>
+            <DropdownMenuLabel className="hover:bg-sidebar-accent cursor-pointer rounded-md" onClick={() => setTheme(theme==='dark' ? 'light' : 'dark')}>{theme==='dark' ? 'Light mode' : 'Dark mode'}</DropdownMenuLabel>
+            <DropdownMenuSeparator  />
+            <DropdownMenuItem className=" cursor-pointer rounded-md">
               Profile settings
             </DropdownMenuItem>
-            <DropdownMenuItem className="focus:bg-zinc-800 focus:text-white cursor-pointer">
+            <DropdownMenuItem className=" cursor-pointer rounded-md">
               Help Center
             </DropdownMenuItem>
-            <DropdownMenuSeparator className="bg-zinc-800" />
-            <DropdownMenuItem className="focus:bg-zinc-800 cursor-pointer text-white hover:text-[#E50914] focus:text-[#E50914]">
-              Sign out of Two Eyes
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer rounded-md  hover:text-[#E50914] focus:text-[#E50914]">
+              Sign out
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
